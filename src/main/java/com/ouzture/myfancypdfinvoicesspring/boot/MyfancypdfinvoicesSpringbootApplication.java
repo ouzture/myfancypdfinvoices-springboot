20f5395a332b08bc576f5b3305763cf6b70b6726
ouzture <ouzture@gmail.com>
2021-01-24 15:30:32 +0300

    added vibur autoconfig support

--- src/main/java/com/ouzture/myfancypdfinvoicesspring/boot/MyfancypdfinvoicesSpringbootApplication.java
+++ src/main/java/com/ouzture/myfancypdfinvoicesspring/boot/MyfancypdfinvoicesSpringbootApplication.java
@@ -1,10 +1,21 @@
 package com.ouzture.myfancypdfinvoicesspring.boot;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class MyfancypdfinvoicesSpringbootApplication {
+
+    @Bean
+    public InitializingBean runner(DataSource dataSource) {
+        return () -> {
+            System.out.println("This is the datasource this Spring Boot project is using: " + dataSource);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MyfancypdfinvoicesSpringbootApplication.class, args);
